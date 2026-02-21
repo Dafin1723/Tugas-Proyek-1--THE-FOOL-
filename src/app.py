1bcabedbf75eebabf818fc0b19beb360b78efc86
Dafin1723 <dafinrifat@gmail.com>
2026-02-21 08:37:10 +0700

    edit tampilan produk

--- src/app.py
+++ src/app.py
@@ -25,5 +25,5 @@ register_routes(app)
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)    
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5003))
     app.run(host='0.0.0.0', port=port, debug=False)
